766a79b3796b3835becb16e356b4517c7e6ad1e7
wjn161 <wjn-161@163.com>
2014-09-23 23:45:09 +0800

    new

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wjn on 9/23/14.
+
+(function($){
+    "use strict";
+    function leftbg(){
+        setInterval(function(){
+            $("#lead").css({
+                'background':'url(http://file12.mafengwo.net/M00/AA/D9/wKgBpU6b5dqD7TZCAAgyy7_oK7w20.jpeg)'
+            });
+        },1000);
+    }
+    leftbg();
+})(jQuery);
+ */
